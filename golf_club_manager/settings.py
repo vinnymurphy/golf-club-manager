223818c447757962b04731f334de0a3c719d6722
Matthew Stibbard <matthewstibbard@gmail.com>
2017-10-04 21:54:05 +0100

    actually fixed static in settings this time.

--- golf_club_manager/settings.py
+++ golf_club_manager/settings.py
@@ -103,8 +103,6 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
